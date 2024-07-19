59e44bd8fff90968fc244c3c77b095a9c3a34368
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-19 16:02:41 -0700

    render condition for show app options

--- App.tsx
+++ App.tsx
@@ -30,10 +30,16 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage as string} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-      </View>
+      {
+        showAppOptions ? (
+         <View /> 
+        ) : (
+          <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+          </View>
+        )
+      }
       <StatusBar style="auto" />
     </View>
   )
