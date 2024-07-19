a545a56ce8b1dfd816e6de4f1d082f406529f1d1
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-19 14:38:03 -0700

    creating button component

---
+++ components/Button.tsx
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+
+export default function Button({ label }: { label: string }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  }
+})
