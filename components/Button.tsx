ed7228197c16a6e63e1acf7032dfb5f3f8a0a6a0
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-19 15:40:59 -0700

    adding new button structure

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,18 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
 
-export default function Button({ label }: Readonly<{ label: string }>) {
+export default function Button({ label, theme }: Readonly<{ label: string, theme: any }>) {
+  if (theme === 'primary') {
+    return (
+      <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }]}>
+        <Pressable style={[styles.button, { backgroundColor: '#FFFFFF' }]} onPress={() => alert('You pressed a button.')}>
+          <FontAwesome name='picture-o' size={18} color="#25292e" style={styles.buttonIcon} />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+        </Pressable>
+      </View>
+    )
+  }
+  
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
