9984279abbe38f4688ffd0dbdc933fb876d23995
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-19 17:09:04 -0700

    adding gesture detector login in emoji sticker component

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,4 +1,6 @@
-import { View, Image } from 'react-native'
+import { View } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
 
 interface EmojiStickerProps {
   stickerSource: any
@@ -6,9 +8,26 @@ interface EmojiStickerProps {
 }
 
 export default function EmojiSticker({ stickerSource, imageSize }: EmojiStickerProps) {
+  const scaleImage = useSharedValue(imageSize)
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) scaleImage.value = scaleImage.value * 2
+    })
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    }
+  })
+
   return (
     <View style={{ top: -350 }}>
-      <Image source={stickerSource} resizeMode='contain' style={{ width: imageSize, height: imageSize }} />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image source={stickerSource} resizeMode='contain' style={[imageStyle, { width: imageSize, height: imageSize }]} />
+      </GestureDetector>
     </View>
   )
 }
